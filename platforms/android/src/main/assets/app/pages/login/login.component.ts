20f3575bdaba46b2c815195c6d07253fc60d6242
Philip Fischbacher <pfischbacher@gmail.com>
2017-03-27 11:20:04 +0900

    tutorial chapter4 ongoing changes

--- platforms/android/src/main/assets/app/pages/login/login.component.ts
+++ platforms/android/src/main/assets/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Color } from "color";
+import { View } from "ui/core/view";
+import { Page } from "ui/page";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 import { User } from "../../shared/user/user";
 import { UserService } from "../../shared/user/user.service";
@@ -10,14 +13,21 @@ import { UserService } from "../../shared/user/user.service";
     styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
     user: User;
     isLoggingIn = true;
 
-    constructor(private router: Router, private userService: UserService) {
+    @ViewChild("container") container: ElementRef;
+
+    constructor(private router: Router, private userService: UserService, private page: Page) {
         this.user = new User();
         this.user.email = "user@nativescript.org";
         this.user.password = "password";
+    }
+
+    ngOnInit () {
+        this.page.actionBarHidden = true;
+        this.page.backgroundImage = "res://bg_login";
     }
 
     submit() {
@@ -49,5 +59,10 @@ export class LoginComponent {
 
     toggleDisplay() {
         this.isLoggingIn = !this.isLoggingIn;
+        let container = <View>this.container.nativeElement;
+        container.animate({
+            backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
+            duration: 200
+        });
     }
 }
